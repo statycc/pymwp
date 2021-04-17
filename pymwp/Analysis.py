d0c447e1082d0db74c0d0939f5fd770cf606295b
Neea Rusch <neearusch@gmail.com>
2021-04-16 21:54:39 -0400

    remove old import

--- pymwp/Analysis.py
+++ pymwp/Analysis.py
@@ -3,8 +3,6 @@ import sys
 import json
 import os
 from pycparser import parse_file, c_parser, c_ast
-# from matrix_utils import RelationList
-# from matrix_utils import Relation
 from matrix import decode
 from relation import Relation
 from relation_list import RelationList
