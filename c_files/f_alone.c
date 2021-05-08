caa6f1891149b0dbe5e9720a1a1c36f4665aadde
Neea Rusch <neearusch@gmail.com>
2021-05-07 20:56:06 -0400

    add composition example

---
+++ c_files/f_alone.c
@@ -0,0 +1,14 @@
+int f(int x_0){
+    int x1;
+    int x2;
+    int x3;
+    x1 = 1;
+    x2 = 2;
+    if(x_0 > 0) {
+        x3 = 1;
+    }
+    else {
+        x3 = x2;
+    }
+    return x3;
+}
---
+++ c_files/f_inline.c
@@ -0,0 +1,18 @@
+int main() {
+    int x;
+    int y;
+    x = 1;
+
+    int x1;
+    int x2;
+    int x3;
+    x1 = 1;
+    x2 = 2;
+    if (x > 0) {
+        x3 = 1;
+    }
+    else {
+        x3 = x2;
+    }
+    y = x3;
+}
