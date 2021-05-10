213bd811ba609031252a3dfba0ffe12c6170b095
Neea Rusch <neearusch@gmail.com>
2021-05-10 11:55:29 -0400

    remove braces to match original example

--- c_files/f_inline.c
+++ c_files/f_inline.c
@@ -8,11 +8,7 @@ int main() {
     int x3;
     x1 = 1;
     x2 = 2;
-    if (x > 0) {
-        x3 = 1;
-    }
-    else {
-        x3 = x2;
-    }
+    if (x > 0) x3 = 1;
+    else x3 = x2;
     y = x3;
 }
