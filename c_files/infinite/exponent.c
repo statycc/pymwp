0d0552d6ba62f1fd2706e164d044f3b58095bd38
caubert <aubert@math.cnrs.fr>
2021-06-08 15:53:01 -0400

    Add simple example with braces issues.

---
+++ c_files/infinite/exponent.c
@@ -0,0 +1,14 @@
+/*
+ * This program tests that a simple program computing the 
+ * exponentiation results in matrix with infinite coefficient in them.
+ */
+
+int main(){
+
+    {
+        if (n % 2 == 1)
+            r *= p;
+        p *= p;
+        n /= 2;
+    }
+}
---
+++ c_files/other/braces_issues.c
@@ -0,0 +1,7 @@
+int main() {
+    int x;
+    int y;
+    {
+    if(x > y){x = y;}
+}
+}
