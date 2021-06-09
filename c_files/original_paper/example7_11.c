12b8da07007ef6a8069b544a258976d375fd351c
caubert <aubert@math.cnrs.fr>
2021-06-09 16:21:04 -0400

    Adding examples from original paper

---
+++ c_files/original_paper/example7_11.c
@@ -0,0 +1,11 @@
+/* 
+ * This example is inspired from Example 7.10
+ * from the original paper "A Flow Calculus of 
+ * mwp-Bounds for Complexity Analysis".
+  */
+
+int main(){
+    X3 = X3 + X4;
+    X2 = X2 + X3;
+    X1 = X1 + X2;
+}
