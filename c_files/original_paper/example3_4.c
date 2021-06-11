0297663398bdc725e6ce9562a076fdd3258dace3
Neea Rusch <neearusch@gmail.com>
2021-06-11 17:27:16 -0400

    update examples

--- c_files/original_paper/example3_4.c
+++ c_files/original_paper/example3_4.c
@@ -6,7 +6,11 @@
 
 int main(){
     while (X1 > 0){
-        X3 = X2 * X2 + X5;
+        // The original example reads
+        // X3 = X2 * X2 + X5;
+        // but we are decomposing that statement in two:
+        X3 = X2 * X2;
+        X3 = X3 + X5;
         X4 = X4 + X5;
     }
 }
