7dfe491c3860a3149ea35761296e2f36ea43aa74
caubert <aubert@math.cnrs.fr>
2021-06-11 16:30:15 -0400

    Removing stack exchange id.

--- c_files/infinite/exponent_2.c
+++ c_files/infinite/exponent_2.c
@@ -2,7 +2,7 @@
  * This program tests that a simple program computing the 
  * exponentiation results in matrix with infinite coefficient in them.
  * 
- * Inspired from https://stackoverflow.com/a/213322/2657549
+ * Inspired from https://stackoverflow.com/a/213322
  */
 
 int main(){
