02429f4cf476168578db6742dd410f976828b68d
caubert <aubert@math.cnrs.fr>
2021-06-19 20:47:57 -0400

    Added example from improvment paper.

---
+++ c_files/improvment_paper/example3.c
@@ -0,0 +1,10 @@
+/* 
+ * This example is inspired from Example 3
+ * from the  paper "An extended and more 
+ *  practical mwp flow analysis".
+  */
+
+int main(){
+    if (0) {X1 = X1 + X2;}
+    else{X1 = X1 - X3;}
+}
