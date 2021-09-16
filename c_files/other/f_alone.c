c30594e21cf3c692085a5ae97683918955be0647
Neea Rusch <neearusch@gmail.com>
2021-09-16 15:12:53 -0400

    update comments

--- c_files/other/f_alone.c
+++ c_files/other/f_alone.c
@@ -1,9 +1,9 @@
 /*
  * This example is a part of a collection of 3 examples:
  *
- * (1) f_alone.c - analysis of a function f in isolation
- * (2) f_inline.c - analysis when function f is inlined within main
- * (3) f_main.c  - analysis of main in isolation, without f
+ * (1) f_alone.c - analysis of a function bar in isolation
+ * (2) f_inline.c - analysis where function bar is inlined within foo
+ * (3) f_main.c  - analysis of foo in isolation, without bar
  */
 
 int bar(int x, int x1, int x2, int x3){
