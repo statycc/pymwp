7633d876184cf51330ab4b12dd42123887a4f038
Neea Rusch <neearusch@gmail.com>
2021-10-05 19:03:16 -0400

    use block comments

--- c_files/other/for_not_body.c
+++ c_files/other/for_not_body.c
@@ -1,5 +1,6 @@
 int foo(int y) {
-    // Should be interpreted as
-    // loop x{y = y + x;}
+    /* Should be interpreted as
+     * loop x{y = y + x;}
+     */
     for (int x = 0; x < 10; x++){y = y + x;}
-} 
+}
