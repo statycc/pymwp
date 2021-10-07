920af93ab80ce0dd36d2d194eee082b91552f8b5
Neea Rusch <neearusch@gmail.com>
2021-10-07 17:23:57 -0400

    update examples

---
+++ c_files/other/complex_dense.c
@@ -0,0 +1,23 @@
+int foo(int X0, int X1, int X2, int X3, int X4){
+    if (X2==1) {
+    X2 = X4 + X3;
+    X3 = X0 + X1;
+    X4 = X1 + X2;
+    X0 = X4 + X3;
+    X1 = X0 + X4;
+    X2 = X1 + X0;
+    X3 = X2 + X1;
+    X4 = X3 + X2;
+    }
+    else
+    {
+    X1 = X4 + X3;
+    X3 = X0 + X2;
+    X4 = X2 + X1;
+    X0 = X4 + X3;
+    X2 = X0 + X4;
+    X1 = X2 + X0;
+    X3 = X1 + X2;
+    X4 = X3 + X1;
+    }
+}
