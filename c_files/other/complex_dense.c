c640ae24b5d4b3e7341574635d197f40feaf3c76
caubert <caubert@augusta.edu>
2021-10-11 10:08:43 -0400

    Messing with the dense examples.

--- c_files/other/complex_dense.c
+++ c_files/other/complex_dense.c
@@ -1,5 +1,14 @@
+/* This example produces a dense matrix
+ * with 43,046,721 different assignments.
+ * It is recommended to analyze it with the 
+ * --no-eval flag.
+ * It takes ~40 s. to analyze this program
+ * without computing the evaluations, 
+ * but ~2 days if the evaluations are computed.
+ */
+
 int foo(int X0, int X1, int X2, int X3, int X4){
-    if (X2==1) {
+    if (X0) {
     X2 = X4 + X3;
     X3 = X0 + X1;
     X4 = X1 + X2;
--- c_files/other/dense.c
+++ c_files/other/dense.c
@@ -1,11 +1,16 @@
+/*
+ * This example produces a 3 x 3
+ * dense matrix.
+ */
+
 int foo(int X0, int X1, int X2){
-    if (X2) {
-    X2 = X2 + X1;
-    X1 = X0 + X1;
-    X0 = X1 + X2;
+    if (X0) {
+    X2 = X0 + X1;
     }
     else
     {
-    X2 = X0 + X1;
+    X2 = X2 + X1;
     }
+    X0 = X2 + X1;
+    X1 = X0 + X2;
 }
