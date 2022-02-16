dfe0a71a7ea251ce44d57682de10ea288a45dae2
Neea Rusch <neearusch@gmail.com>
2022-02-15 19:18:44 -0500

    -b param

--- c_files/implementation_paper/example15_b.c
+++ c_files/implementation_paper/example15_b.c
@@ -7,7 +7,7 @@
  * In this example function call is skipped by in-lining the function.
  */
 
-int foo(int X1, int X2, int b, int R) {
+int foo(int X1, int X2, int R) {
     X2 = X1 + X1;
     X_1 = X2;  /* rename input variables */
     R = X2;    /* rename returned variable */
