6c297677fb998dde815bf57e29bfd76d257c9a46
Neea Rusch <neearusch@gmail.com>
2022-02-15 20:24:49 -0500

    match paper

--- c_files/implementation_paper/example15_b.c
+++ c_files/implementation_paper/example15_b.c
@@ -7,10 +7,9 @@
  * In this example function call is skipped by in-lining the function.
  */
 
-int foo(int X1, int X2, int R) {
+int foo(int X1, int X2, int X_1, int R) {
     X2 = X1 + X1;
     X_1 = X2;  /* rename input variables */
-    R = X2;    /* rename returned variable */
     while(b) { R = X_1 + X_1; }  /* chunk */
     X1 = R;    /* return is removed */
 }
