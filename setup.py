aa2de45d9f2b8b0982c49f8798050d08c5d9894c
Neea <neearusch@gmail.com>
2023-04-30 00:43:36 -0400

    citation files

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setuptools.setup(
         'Typing :: Typed',
         'Topic :: Scientific/Engineering',
         'Topic :: Software Development',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
     ],
     python_requires=">=3.7",
     install_requires=[
