3b64373be4ea0aa0fe7a13a3f940fefcf82b9ad1
Neea <neearusch@gmail.com>
2024-05-06 17:48:12 -0400

    fix raised comment issue

--- pymwp/analysis.py
+++ pymwp/analysis.py
@@ -418,8 +418,9 @@ class Analysis:
             relations.composition(rel_list)
 
         relations.fixpoint()
-        # TODO: unknown method conditionRel
-        #  ref: https://github.com/statycc/pymwp/issues/5
+        # This is not done, and I know it
+        # ref: https://github.com/statycc/pymwp/issues/5
+        # unknown method conditionRel
         # relations = relations.conditionRel(VarVisitor.list_var(node.cond))
         return index, relations, False
 
