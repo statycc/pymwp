ede51c1776f1cb610dae51229ed366a57687b0be
Neea <neearusch@gmail.com>
2024-09-07 20:07:42 -0400

    bound equals

--- tests/test_bound.py
+++ tests/test_bound.py
@@ -10,6 +10,30 @@ def test_bound_triple():
     assert mwp.bound_triple == (('X0',), ('X1',), ('X2',))
 
 
+def test_mwp_bound_equals():
+    mwp = MwpBound()
+    mwp.append('m', 'X0')
+    mwp.append('m', 'X2')
+    mwp.append('m', 'X1')
+    mwp.append('w', 'X4')
+    mwp.append('p', 'X8')
+    mwp.append('p', 'X5')
+    mwp.append('p', 'X6')
+
+    assert mwp == MwpBound("X1,X0,X2;X4;X6,X5,X8")
+
+
+def test_bound_not_equals():
+    b1 = Bound({'X3': "X3,X5;;X1,X2",
+                'X4': "X4;;X1,X5"})
+    b2 = Bound({'X3': "X3,X2;;X1,X5",
+                'X4': "X4;;X1,X5"})
+    assert b1.variables == b2.variables
+    assert b1.bound_dict['X4'] == b2.bound_dict['X4']
+    assert b1.bound_dict['X3'] != b2.bound_dict['X3']
+    assert b1 != b2
+
+
 def test_bound_str_format():
     mwp = MwpBound()
     mwp.x.add('X0', 'X1')
