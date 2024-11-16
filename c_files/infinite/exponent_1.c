5157a50279aed3c5c962761042a28efd6200f2d5
Neea <neearusch@gmail.com>
2024-11-16 17:22:39 -0500

    eliminate division in exponent_1.c

--- c_files/infinite/exponent_1.c
+++ c_files/infinite/exponent_1.c
@@ -2,15 +2,20 @@
  * This program tests that a simple program computing the 
  * exponentiation results in matrix with infinite coefficient in them.
  * Inspired from https://stackoverflow.com/a/213897
+ * Lines 14--19 refactors n=n/2 to eliminate the division operator.
  */
-
-int main(int x, int n, int p, int r){
+int main(int x, int n, int p, int r, int d){
     p = x;
     while (n > 0)
     {
         if (n % 2 == 1)
             r = p * r;
         p = p * p;
-        n = n / 2;
+        d = 0;
+        while (n > 0){
+            n = n - 2;
+            d = d + 1;
+        }
+        n = d;
     }
 }
